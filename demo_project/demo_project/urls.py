7fda0a84bfca43545464442aa036d38d3c96c806
Ali Zaidi <alixedi@bitswits.com>
2013-12-09 10:48:58 +0500

    Added support for permissions
    The add-another link now will only appear if the user has add
    permission for the model in question.

--- demo_project/demo_project/urls.py
+++ demo_project/demo_project/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.contrib.auth.models import User, Group, Permission
 from django.views.generic import CreateView
 
@@ -6,18 +6,18 @@ from popcorn.viewmixins import PopcornMixin
 from popcorn.utils import get_popcorn_urls
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 class CreateUser(CreateView):
     model = User
 
 class PopcornGroup(PopcornMixin,
-                  CreateView):
+                   CreateView):
     model = Group
 
 class PopcornPermission(PopcornMixin,
-                  CreateView):
+                        CreateView):
     model = Permission
 
 urlpatterns = patterns('',
@@ -29,7 +29,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += get_popcorn_urls([PopcornGroup, PopcornPermission])
--- popcorn/templatetags/popcorn.py
+++ popcorn/templatetags/popcorn.py
@@ -1,27 +1,17 @@
 from django import template
-from django.utils.safestring import mark_safe
-from django.core.urlresolvers import reverse
+from django.template.loader import get_template
 
 
 register = template.Library()
 
-@register.filter(is_safe=True, needs_autoescape=True)
-def get_popup_link(field, autoescape=None):
-    """
-    Returns link to popups. Used in FormViews to put in + for
-    ForeignKey fields. Field here refer to field of forms.
-    """
+@register.simple_tag(takes_context=True)
+def popcorn(context, field):
     model = field.field.queryset.model
     model_name = model._meta.object_name.lower()
     app_name = model._meta.app_label.lower()
     view_name = '%s_%s_popcorn' % (app_name, model_name)
-    link = ""
-    try:
-        href = reverse(view_name)
-        link = mark_safe("<a href='%s' \
-            class='add-another' id='add_id_%s' \
-            onclick='return showAddAnotherPopup(this);'> + </a>" \
-            % (href, field.name))
-    except:
-        pass
-    return link
+    perm = '%s.add_%s' % (app_name, model_name)
+    context['field_name'] = field.name
+    context['view_name'] = view_name
+    context['has_perm'] = perm in context['perms']
+    return get_template('popcorn/link.html').render(context)
